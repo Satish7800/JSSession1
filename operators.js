ddc1615471c02eea4a237bbf0fd0e15969d7c4d0
Satish7800 <88663964+Satish7800@users.noreply.github.com>
2026-07-06 13:03:43 +0530

    Operators and Stack and Heap

---
+++ operators.js
@@ -0,0 +1,159 @@
+// console.log("1"+2);
+// console.log(2+"1");
+// console.log(1+1+"2");
+// console.log("1"+1+2);
+
+// console.log((3 + 4) * 5 % 3);
+
+
+//The + operator has two behaviors:
+
+// =>Addition (when both operands are numbers)
+    // 5 + 3 // 8
+// =>String concatenation (when either operand is a string)
+// "5" + 3 // "53"
+
+// Operators and Operands 
+//Arthimetic Operators
+
+ 
+// let x = 10;
+// let y = 15;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y); 
+// console.log(x / y); 
+// console.log(x % y); 
+// console.log(x ** y);
+
+
+// console.log(2 ** 4);
+
+// Assignment Operators
+ 
+
+//=	 let  x = 5;              
+// +=	 let  x += 5  same as x=x+5	          
+// -=	 let  x -= 5  same as x=x-5   	      
+// *=	 let  x *= 5  same as x=x*5         
+// /=	 let  x /= 5  same as x=x/5          
+// %=	 let  x %= 5  same as x=x%5 
+
+// let  x = 5; y = 10;
+// console.log(x);
+// x+=5
+// x=x+5;
+// console.log(x);
+
+// console.log(x);
+// x-=5
+// x=x-5;
+// console.log(x);
+
+// console.log(x);
+//  x*=5
+// x=x*5;
+// console.log(x);
+
+
+// console.log(x);
+// x/=5
+// x=x/5;
+// console.log(x);
+
+// console.log(x);
+// x%=5
+// x=x%5;
+// console.log(x);
+
+
+
+// Comparison Operators
+
+// ==	Equal (value only)	
+// ===	Strict Equal	
+// !=	Not Equal	
+// !==	Strict Not Equal	
+// >	Greater Than	
+// <	Less Than	
+// >=	Greater or Equal	
+// <=	Less or Equal
+
+// let  x = 5; y = 5;
+
+// console.log(x==y);
+// console.log(x===y);
+// console.log(x!=y);
+// console.log(x!==y);
+// console.log(x>y);
+// console.log(x<y);
+
+// console.log(x>=y);
+// console.log(x<=y);
+
+
+// Logical Operators
+
+// &&  
+// ||
+// !
+// let x;y=10;
+
+// console.log(!x);
+
+
+
+// Unary Operators
+
+// --
+// ++
+// let x=5;
+
+// let x = 7 , y = 10;
+
+// console.log(--x);
+// console.log(x);
+
+
+
+// Ternary Operator
+
+// ?
+
+// let age=17;
+// let result;
+
+// result = age >= 18 ? "Adult" : "Minor"; 
+// console.log(result);
+
+
+// Stack (Premitive) and Heap (Non Premitive)
+
+// let userName="Rajesh";
+// let userName2=userName;
+//     userName2="Suresh";
+
+// console.log(userName)
+// console.log(userName2);
+
+// Ojbect
+
+// let user = {
+//     email:"ram@gmail.com",
+//     mobile:"7899777884"
+// }
+
+// let user2 = user;
+
+// user2.email="rrrr@gmail.com";
+
+// console.log(user.email);
+// console.log(user2.email);
+
+
+
+
+
+
+
