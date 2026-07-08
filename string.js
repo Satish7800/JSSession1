a1f37c76c0a85bd78862f7a834ed3f1925df3cc4
Satish7800 <88663964+Satish7800@users.noreply.github.com>
2026-07-08 16:49:02 +0530

    String

---
+++ string.js
@@ -0,0 +1,71 @@
+
+// In JavaScript, a string is a primitive data type used to represent text. 
+// Strings can be created using single quotes (' '), double quotes (" "), or backticks (` `).
+
+// let name2="JavaScript";
+// let str = "abcd";
+// let name = new String("Umesh");
+
+// console.log(name2.length);
+// console.log(name2.charAt(2))
+// console.log(name2.indexOf("s"))
+// console.log(name2.toLowerCase());
+// console.log(name2.toUpperCase());
+
+// console.log(name2.substring(-4,4));
+// console.log(str.slice(-4,4));
+
+// let str = "JavaScript";
+// console.log(str.includes("Java"));
+
+// let str2="hello,hi,bey";
+// console.log(str.replace("Java", "Hello"))  
+// let str3 = str2.split(",")
+// console.log(str3[1]) 
+
+
+// let a = "Hello";
+// let b = new String("Hello");
+
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+// console.log(a==b);
+// console.log(a===b);
+
+
+// Note:- "==" Converts the String object to its primitive value before comparing.
+// console.log(a==b);
+
+
+// Note:- Primitive (string) and Object are different types.
+// console.log(a===b);
+
+// String Interpolation in JavaScript
+
+// let price = 100;
+// let quantity = 5;
+
+// console.log(`Total Price = ${price * quantity}`);
+
+
+// Without String Interpolation
+
+// console.log("Total Price = " + price * quantity);
+
+
+// Note: Strings are immutable in JavaScript 
+// (methods return a new string instead of modifying the original).
+
+
+// Escape Characters
+
+// console.log("Hello\nWorld");
+
+// console.log("Name\t\tAge");
+
+// console.log("He said \"Hello\"");
+
+
+
